40ab6799fff38bc490775e9ddb5409e514dfc68c
Ale110 <pooh110andco@ya.ru>
2013-02-12 21:49:38 -0800

    Fixed :3

--- brackets.ml
+++ brackets.ml
@@ -13,7 +13,7 @@ let parse str_ =
             failwith "parse_s [ error"
           else
             let (j2, k2) = parse_s (j1 + 1) in
-              (j2+1, [Square k1] @ k2)
+              (j2, [Square k1] @ k2)
 
       | '(' ->
         let (j1, k1) = (parse_s (i + 1)) in
@@ -21,7 +21,7 @@ let parse str_ =
             failwith "parse_s ( error"
           else
             let (j2, k2) = parse_s (j1 + 1) in
-              (j2+1, [Round k1] @ k2)
+              (j2, [Round k1] @ k2)
 
       | _ -> (i, [])
 
@@ -30,14 +30,23 @@ let parse str_ =
 
 (* DEBUG *)
 
-let print_list f lst =
-  print_string "[";
-  List.iter (fun x -> (f x; print_string "; ")) lst;
-  print_string "]";;
+let print_list f lst = List.iter (fun x -> (f x)) lst;;
 
 let rec print_tree t =
   match t with
-    | Round l ->  print_string "Rnd"; print_list (print_tree) l
-    | Square l -> print_string "Sqr"; print_list (print_tree) l;;
+    | Round l ->  print_string "("; print_list (print_tree) l; print_string ")"
+    | Square l -> print_string "["; print_list (print_tree) l; print_string "]";;
 
-print_list (print_tree) (parse "([])");
+print_list (print_tree) (parse "(([[]](())[[]]))");
+
+
+
+
+
+
+
+
+
+
+
+
